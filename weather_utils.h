3f5cd3afb1a874dbf7ba9ebf266c4eb671270060
Manish Kumar Mandal <kmanishm7@gmail.com>
2026-04-03 19:57:47 +0530

    UI updates
    Dhanush this is better UI

--- weather_utils.h
+++ weather_utils.h
@@ -1,6 +1,11 @@
 #ifndef WEATHER_UTILS_H
 #define WEATHER_UTILS_H
 
+#include <Arduino.h>
+
 const char* getWeatherStatus(float temperature);
+const char* getHumidityStatus(float humidity);
+float calculateFeelsLikeC(float temperature, float humidity);
+void printWeatherDashboard(float temperature, float humidity);
 
 #endif
